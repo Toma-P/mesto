20a2ff058c4d7e5db930b410dc707142e023f861
Tamara Pashkova <summer_princess@mail.ru>
2023-02-02 21:46:57 +0300

    fix: исправить скрипт формы и мелкие ошибки разметки

--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,8 @@ const closeButton = popup.querySelector('.popup__close-button');
 const profileForm = popup.querySelector('.popup__form');
 let username = profileForm.querySelector('.popup__form-item_type_username');
 let about = profileForm.querySelector('.popup__form-item_type_about');
+let profileName = document.querySelector('.profile__info-title');
+let profileAbout = document.querySelector('.profile__info-subtitle');
 
 function toggleOpenPopup() {
   popup.classList.toggle('popup_opened');
@@ -11,36 +13,24 @@ function toggleOpenPopup() {
 
 function handleEditButtonClick() {
   toggleOpenPopup();
-  username.value = document.querySelector('.profile__info-title').textContent;
-  about.value = document.querySelector('.profile__info-subtitle').textContent;
-  username.setAttribute('value', username.value);
-  about.setAttribute('value', about.value); 
+  username.value = profileName.textContent;
+  about.value = profileAbout.textContent;
 }
 
-function handleOverlayClick(event) {
-  if(event.target === event.currentTarget) {
-    toggleOpenPopup();
-  }
-}
+//function handleOverlayClick(event) {
+//  if(event.target === event.currentTarget) {
+//    toggleOpenPopup();
+//  }
+//}
 
 function handleEditProfile(event) {
   event.preventDefault();
-  if(!username.value || !about.value) {
-    toggleOpenPopup();
-  }
-  else {
-    let editName = username.value;
-    let editAbout = about.value;
-    console.log(editName);
-    username.setAttribute('value', editName);
-    about.setAttribute('value', editAbout);
-    document.querySelector('.profile__info-title').textContent = username.value;
-    document.querySelector('.profile__info-subtitle').textContent = about.value;
-    toggleOpenPopup();
-  }
+  profileName.textContent = username.value;
+  profileAbout.textContent = about.value;
+  toggleOpenPopup();
 }
 
 editButton.addEventListener('click', handleEditButtonClick);
 closeButton.addEventListener('click', toggleOpenPopup);
-popup.addEventListener('click', handleOverlayClick);
 profileForm.addEventListener('submit', handleEditProfile);
+//popup.addEventListener('click', handleOverlayClick);
